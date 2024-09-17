87d961cfb578364e8d6fa346c7f2f6191feeafc6
Arctic Fox <36272979+DraculaSide@users.noreply.github.com>
2024-09-17 20:30:02 +0200

    Add documentation for ShapePos and IRitual interfaces
    Enhanced documentation by adding detailed class-level and method-level comments for the ShapePos and IRitual interfaces. This improves code readability and provides clear insights into the purpose and functionality of each component.

--- src/main/kotlin/de/butterfly/butterflyapi/util/ShapePos.kt
+++ src/main/kotlin/de/butterfly/butterflyapi/util/ShapePos.kt
@@ -1,7 +1,21 @@
 package de.butterfly.butterflyapi.util
 
+/**
+ * Represents a position in a 3D space, with X, Z coordinates and a layer.
+ *
+ * @property X The X coordinate.
+ * @property Z The Z coordinate.
+ * @property LAYER The layer which denotes the height level.
+ * original Author @author SarahGreyWolf
+ */
+@Suppress("unused")
 data class ShapePos(var X: Int = 0, var Z: Int = 0, var LAYER: Int = 0) {
 
+    /**
+     * Checks if all the properties of the ShapePos instance are zero.
+     *
+     * @return true if X, Z, and LAYER are all zero; false otherwise.
+     */
     fun isZero(): Boolean {
         return X == 0 && Z == 0 && LAYER == 0
     }
