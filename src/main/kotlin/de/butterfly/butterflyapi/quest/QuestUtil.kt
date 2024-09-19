6cff6383f4ccf179b3f870b92de86a735f212e1b
Arctic Fox <36272979+DraculaSide@users.noreply.github.com>
2024-09-19 19:25:08 +0200

    Add Quest model and QuestUtil class
    Implemented the data model for quests including various attributes such as name, description, and reward details. Introduced QuestUtil class to manage quest progress and status.

---
+++ src/main/kotlin/de/butterfly/butterflyapi/quest/QuestUtil.kt
@@ -0,0 +1,8 @@
+package de.butterfly.butterflyapi.quest
+
+class QuestUtil {
+    var questID:Int = 0
+    var isQuestCompleted:Boolean = false
+    val questProgress:MutableMap<Int,Boolean> = mutableMapOf()
+    fun
+}
