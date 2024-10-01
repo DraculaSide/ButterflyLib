59db4aa47b24d9988960a6bc035860e40c777b56
Arctic Fox <36272979+DraculaSide@users.noreply.github.com>
2024-10-01 11:49:34 +0200

    Delete unused classes and annotations
    Removed `FusionSkill`, `IRitualListener`, `RitualInformation`, `SkillInformation`, and `WrittenBook` from the codebase. These components were not being utilized and their removal helps in streamlining the project.

--- src/main/kotlin/de/butterfly/butterflylibrary/annotation/RitualInformation.kt
+++
@@ -1,6 +0,0 @@
-package de.butterfly.butterflylibrary.annotation
-
-annotation class RitualInformation(
-    val name: String,
-    val description: String,
-)
--- src/main/kotlin/de/butterfly/butterflylibrary/ritual/IRitualListener.kt
+++
@@ -1,9 +0,0 @@
-package de.butterfly.butterflylibrary.ritual
-
-import org.bukkit.event.Event
-@Suppress("unused")
-class IRitualListener {
-    fun onPlayDropItem(event: Event) {}
-    fun onBlockIgnite(event: Event) {}
-    fun onTreeGrow(event: Event) {}
-}
